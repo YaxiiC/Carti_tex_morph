2874b1731ae8a2c8cbcfe34accd7a27fb7e349b1
Yaxi <164341125+YaxiiC@users.noreply.github.com>
2024-10-23 12:42:18 +0100

    Update extract_features.py

--- extract_features.py
+++ extract_features.py
@@ -12,39 +12,39 @@ def save_features(features, labels, idx, output_dir):
     np.save(feature_path, features.numpy())
     np.save(label_path, labels.numpy())
 
-def main():
+#def main():
     #root_dir = "/Users/chrissychen/Documents/PhD_2nd_year/miccai2025/MRNet-v1.0"  
-    root_dir = "/home/yaxi/MRNet-v1.0_gpu" 
-    output_dir = "/home/yaxi/Carti_tex_morph/extracted_features"
-    os.makedirs(output_dir, exist_ok=True)
+#    root_dir = "/home/yaxi/MRNet-v1.0_gpu" 
+#    output_dir = "/home/yaxi/Carti_tex_morph/extracted_features"
+#    os.makedirs(output_dir, exist_ok=True)
     
-    labels_files = {
-        'abnormal': os.path.join(root_dir, 'train-abnormal.csv'),
-        'acl': os.path.join(root_dir, 'train-acl.csv'),
-        'meniscus': os.path.join(root_dir, 'train-meniscus.csv')
-    }
+#    labels_files = {
+#        'abnormal': os.path.join(root_dir, 'train-abnormal.csv'),
+#        'acl': os.path.join(root_dir, 'train-acl.csv'),
+#        'meniscus': os.path.join(root_dir, 'train-meniscus.csv')
+#    }
 
     # Load the dataset
-    dataset = MRNetDataset(root_dir=root_dir, phase='train', view='coronal', labels_files=labels_files)
+#    dataset = MRNetDataset(root_dir=root_dir, phase='train', view='coronal', labels_files=labels_files)
 
     # Set up DataLoader
-    data_loader = DataLoader(dataset, batch_size=1, shuffle=False)
+#    data_loader = DataLoader(dataset, batch_size=1, shuffle=False)
 
     # Initialize the feature extractor
-    feature_extractor = FeatureExtractor()
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    feature_extractor.to(device)
+#    feature_extractor = FeatureExtractor()
+#    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+#    feature_extractor.to(device)
 
     # Extract features and save them
-    for idx, (image, labels) in enumerate(data_loader):
-        image, labels = image.to(device), labels.to(device)
-        features = feature_extractor(image.squeeze(0)) 
+#    for idx, (image, labels) in enumerate(data_loader):
+#        image, labels = image.to(device), labels.to(device)
+#        features = feature_extractor(image.squeeze(0)) 
 
         # Save features and labels
-        save_features(features, labels.squeeze(0), idx, output_dir)
+#        save_features(features, labels.squeeze(0), idx, output_dir)
 
-        if idx % 100 == 0:
-            print(f"Processed {idx} images")
+#        if idx % 100 == 0:
+#            print(f"Processed {idx} images")
 
-if __name__ == "__main__":
-    main()
+#if __name__ == "__main__":
+#    main()
